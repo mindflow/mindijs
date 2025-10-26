4748f31f098546d26c23a858d93c23054ea1f9f6
torbjorn.nilsen@gmail.com <torbjorn.nilsen@gmail.com>
2025-10-26 18:39:16 +1100

    Refactor, remove COMPONENT_NAME.

--- src/mindi/typeConfig/typeConfig.js
+++ src/mindi/typeConfig/typeConfig.js
@@ -5,9 +5,20 @@ export class TypeConfig {
     static get NEW() { return "NEW"; }
     static get CONFIGURED() { return "CONFIGURED"; }
 
+    /**
+     * 
+     * @param {String} name 
+     * @param {Function} classReference 
+     */
     constructor(name, classReference) {
+
+        /** @type {String} */
         this.name = name;
+
+        /** @type {Function} */
         this.classReference = classReference;
+
+        /** @type {String} */
         this.stage = TypeConfig.NEW;
     }
 
